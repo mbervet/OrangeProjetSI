fa88494a7b4a9a407f0e6a2cf8c6062e31ce5bd4
Maël Bervet <mael.bervet@gmail.com>
2019-09-20 15:29:28 +0200

    test import package

---
+++ OrangeSIProject/TestImportPackage/__init__.py
@@ -0,0 +1,7 @@
+NAME = "TestImportPackage"
+
+DESCRIPTION = "test to include package"
+
+BACKGROUND = "#C0FF97"
+
+ICON = "icons/test.svg"
--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     packages=["OrangeSIProject"],
     package_data={"OrangeSIProject": ["icons/*.svg"]},
     classifiers=["Example :: Invalid"],
-    # Declare orangedemo package to contain widgets for the "Demo" category
     entry_points={"orange.widgets": "SIProject = OrangeSIProject"},
 )
